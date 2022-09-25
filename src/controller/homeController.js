466a98f0ad0fda2c01fe7f7a190157516172b42a
Quang Huy <nguyenquanghuya3kd@gmail.com>
2022-09-25 17:27:24 +0700

    sixth commit

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,34 +1,25 @@
-import connection from '../configs/connectDB';
+import pool from '../configs/connectDB';
 
 
-let getHomePage = (req,res)=>{
-    let data = [];
-    connection.query(
-    'SELECT * FROM `users`',
-    function(err, results, fields) {
-        console.log('check sql')
-        console.log(results); // results contains rows returned by server
-        results.map((row) => { 
-            data.push({
-                ID : row.ID,
-                firstName : row.firstName,
-                lastName : row.lastName,
-                email : row.email,
-                address : row.address
-        })
-            
-        });
-          // console.log(fields); // fields contains extra meta data about results, if available
-        // console.log(rows);
-        return res.render('index.ejs',{dataUser: JSON.stringify(data)});
-        });
+let getHomePage = async (req,res)=>{
+
+    const [rows, fields] = await pool.execute( 'SELECT * FROM `users`');
+    return res.render('index.ejs',{dataUser: rows});
    
 }
 
 let getAboutPage = (req,res)=>{
     return res.render('about.ejs');
 }
+
+let getDetailPage = async (req,res) =>{
+    let ID = req.params.ID;
+    let [user] = await pool.execute('SELECT * FROM `users` WHERE ID=?', [ID])
+
+    return res.send(JSON.stringify(user[0]));
+}
 module.exports = {
     getHomePage,
-    getAboutPage
+    getAboutPage,
+    getDetailPage
 }
--- src/route/web.js
+++ src/route/web.js
@@ -5,6 +5,7 @@ let router = express.Router();
 const initWebRoute = (app)=>{
     router.get('/', homeController.getHomePage)
     router.get('/about', homeController.getAboutPage)
+    router.get('/detail/user/:ID', homeController.getDetailPage)
     return app.use('/', router);
 }
 module.exports = initWebRoute;
